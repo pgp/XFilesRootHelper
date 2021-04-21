c920c847ed4f928591df948e4b6e06c4358f2905
PGP <pgp@gmx.it>
2021-04-21 21:17:05 +0200

    First implementation of unified build script for windows

---
+++ build.bat
@@ -0,0 +1,48 @@
+@ECHO OFF
+
+:: main entry point
+IF "%1" == "-f" (
+    call :fullbuild
+) ELSE (
+    IF "%1" == "" (
+        call :stdbuild
+    ) ELSE (
+        echo Only -f option is allowed - for full build
+		EXIT /B 1
+    )
+)
+EXIT /B %ERRORLEVEL%
+
+
+:: detect cpu number
+:detectcpus
+IF "%NUMBER_OF_PROCESSORS%"=="" ( 
+    EXIT /B 2
+) ELSE (
+    EXIT /B %NUMBER_OF_PROCESSORS%
+)
+
+
+:: standard build
+:stdbuild
+cd CMAKE
+call :detectcpus
+cmake --build winbuild -- -j%ERRORLEVEL%
+robocopy ..\cert ..\bin /s /e
+cd ..
+EXIT /B 0
+
+
+:: full build
+:fullbuild
+cd CMAKE
+md ..\bin
+rd /S /Q winbuild
+set CC=c:\MinGW\bin\gcc.exe
+set CXX=c:\MinGW\bin\g++.exe
+cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -H. -Bwinbuild
+call :detectcpus
+cmake --build winbuild -- -j%ERRORLEVEL%
+robocopy ..\cert ..\bin /s /e
+cd ..
+EXIT /B 0
--- genRootHelperDesktop.bat
+++
@@ -1,6 +0,0 @@
-cd CMAKE
-cmake --build winbuild -- -j4
-
-robocopy ..\cert ..\bin /s /e
-
-cd ..
